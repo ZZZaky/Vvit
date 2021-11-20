2a100b8cd4e5396eb2977034a4eb410f5d9f0d6c
ZZZaky <yurakalinin29@gmail.com>
2021-11-20 13:45:39 +0300

    Azure and deleting cache

--- Triangular/main.py
+++ Triangular/main.py
@@ -1,19 +1,19 @@
-list = []
-for i in range(50):
-    list.append(i+1)
+# list = []
+# for i in range(50):
+#     list.append(i+1)
 
-list.sort(reverse=True)
-first = list[0]
-second = list[1]
-s = 0
-p = 0
-s_max = 0
-maxim = []
-for i in range(2, len(list)):
-    if second + list[i] > first and first + second > list[i] and first + list[i] > second:
-        p = (first + second + list[i]) / 2
-        s = (p*(p-first)*(p-second)*(p-list[i])) ** 0.5
-        maxim = [first, second, list[i]]
-        break
+# list.sort(reverse=True)
+# first = list[0]
+# second = list[1]
+# s = 0
+# p = 0
+# s_max = 0
+# maxim = []
+# for i in range(2, len(list)):
+#     if second + list[i] > first and first + second > list[i] and first + list[i] > second:
+#         p = (first + second + list[i]) / 2
+#         s = (p*(p-first)*(p-second)*(p-list[i])) ** 0.5
+#         maxim = [first, second, list[i]]
+#         break
 
-print(s, "  ", maxim)
+# print(s, "  ", maxim)
