8192eeb70ddd491cf22f1808c430f2edd486925d
ZZZaky <yurakalinin29@gmail.com>
2021-11-22 19:53:03 +0300

    sm ch

--- Azure/app.py
+++ Azure/app.py
@@ -6,47 +6,46 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
+
 app = Flask(__name__)
 
-
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        original_text = request.form['text']
+        target_language = request.form['language']
 
-@app.route('/', methods=['POST'])
-def index_post():
-    original_text = request.form['text']
-    target_language = request.form['language']
+        key = os.environ['KEY']
+        endpoint = os.environ['ENDPOINT']
+        location = os.environ['LOCATION']
 
-    key = os.environ['KEY']
-    endpoint = os.environ['ENDPOINT']
-    location = os.environ['LOCATION']
+        path = '/translate?api-version=3.0'
 
-    path = '/translate?api-version=3.0'
+        target_language_parameter = '&to=' + target_language
 
-    target_language_parameter = '&to=' + target_language
+        constructed_url = endpoint + path + target_language_parameter
 
-    constructed_url = endpoint + path + target_language_parameter
+        headers = {
+            'Ocp-Apim-Subscription-Key': key,
+            'Ocp-Apim-Subscription-Region': location,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
 
-    headers = {
-        'Ocp-Apim-Subscription-Key': key,
-        'Ocp-Apim-Subscription-Region': location,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-    }
+        body = [{'text': original_text}]
 
-    body = [{'text': original_text}]
+        translator_request = requests.post(
+            constructed_url, headers=headers, json=body)
 
-    translator_request = requests.post(
-        constructed_url, headers=headers, json=body)
+        translator_response = translator_request.json()
 
-    translator_response = translator_request.json()
+        translated_text = translator_response[0]['translations'][0]['text']
 
-    translated_text = translator_response[0]['translations'][0]['text']
-
-    return render_template(
-        'results.html',
-        translated_text=translated_text,
-        original_text=original_text,
-        target_language=target_language
-    )
+        return render_template(
+            'results.html',
+            translated_text=translated_text,
+            original_text=original_text,
+            target_language=target_language
+        )
+    elif request.method == 'GET':
+        return render_template('index.html')
