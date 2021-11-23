bcfde7faeabb8a44b853102a5368f8383fde6c03
ZZZaky <yurakalinin29@gmail.com>
2021-11-23 14:27:23 +0300

    Calculator

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -2,63 +2,93 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QPushButton
 
 class Calculator(QWidget):
+    op = ""
     def __init__(self):
-        super(Calculator, self).__init__()
+        super().__init__()
         
-        #задаем строки
+        #create grid
         self.vbox = QVBoxLayout(self)
+        
         self.hbox_input = QHBoxLayout()
+        
         self.hbox_first = QHBoxLayout()
-        self.hbox_operations = QHBoxLayout()
-        self.hbox_result = QHBoxLayout()
-
+        self.hbox_second = QHBoxLayout()
+        self.hbox_third = QHBoxLayout()
+        self.hbox_fourth = QHBoxLayout()
+        self.hbox_fifth = QHBoxLayout()
+        
         self.vbox.addLayout(self.hbox_input)
         self.vbox.addLayout(self.hbox_first)
-        self.vbox.addLayout(self.hbox_operations)
-        self.vbox.addLayout(self.hbox_result)
-
-        #создаем поле ввода
+        self.vbox.addLayout(self.hbox_second)
+        self.vbox.addLayout(self.hbox_third)
+        self.vbox.addLayout(self.hbox_fourth)
+        self.vbox.addLayout(self.hbox_fifth)
+        
+        #create input line
         self.input = QLineEdit(self)
         self.hbox_input.addWidget(self.input)
-
-        #создаем кнопки
-        self.b_1 = QPushButton("1", self)
-        self.hbox_first.addWidget(self.b_1)
-        self.b_2 = QPushButton("2", self)
-        self.hbox_first.addWidget(self.b_2)
-        self.b_3 = QPushButton("3", self)
-        self.hbox_first.addWidget(self.b_3)
-        self.b_4 = QPushButton("4", self)
-        self.hbox_first.addWidget(self.b_4)        
-        self.b_5 = QPushButton("5", self)
-        self.hbox_first.addWidget(self.b_5)        
-        self.b_6 = QPushButton("6", self)
-        self.hbox_first.addWidget(self.b_6)        
-        self.b_7 = QPushButton("7", self)
-        self.hbox_first.addWidget(self.b_7)        
-        self.b_8 = QPushButton("8", self)
-        self.hbox_first.addWidget(self.b_8)        
-        self.b_9 = QPushButton("9", self)
-        self.hbox_first.addWidget(self.b_9)
-        self.b_0 = QPushButton("0", self)
-        self.hbox_first.addWidget(self.b_0)
-
-        self.b_plus = QPushButton("+", self)
-        self.hbox_operations.addWidget(self.b_plus)        
-        self.b_minus = QPushButton("-", self)
-        self.hbox_operations.addWidget(self.b_minus)        
-        self.b_multiply = QPushButton("*", self)
-        self.hbox_operations.addWidget(self.b_multiply)        
+        
+        #create buttons
+        #
+        #     input line
+        #    C   %  del  /      first row
+        #    7   8   9   *      second row
+        #    4   5   6   -      third row
+        #    1   2   3   +      fourth row
+        #    00  0   ,   =      fifth row
+        #
+        #1 row
+        self.b_reset = QPushButton("C", self)
+        self.hbox_first.addWidget(self.b_reset)
+        self.b_percent = QPushButton("%", self)
+        self.hbox_first.addWidget(self.b_percent)
+        self.b_delete = QPushButton("del", self)
+        self.hbox_first.addWidget(self.b_delete)
         self.b_divide = QPushButton("/", self)
-        self.hbox_operations.addWidget(self.b_divide)
+        self.hbox_first.addWidget(self.b_divide)
         
+        #2 row
+        self.b_7 = QPushButton("7", self)
+        self.hbox_second.addWidget(self.b_7)
+        self.b_8 = QPushButton("8", self)
+        self.hbox_second.addWidget(self.b_8)
+        self.b_9 = QPushButton("9", self)
+        self.hbox_second.addWidget(self.b_9)
+        self.b_multiply = QPushButton("*", self)
+        self.hbox_second.addWidget(self.b_multiply)
+        
+        #3 row
+        self.b_4 = QPushButton("4", self)
+        self.hbox_third.addWidget(self.b_4)
+        self.b_5 = QPushButton("5", self)
+        self.hbox_third.addWidget(self.b_5)
+        self.b_6 = QPushButton("6", self)
+        self.hbox_third.addWidget(self.b_6)
+        self.b_minus = QPushButton("-", self)
+        self.hbox_third.addWidget(self.b_minus)
+        
+        #4 row
+        self.b_1 = QPushButton("1", self)
+        self.hbox_fourth.addWidget(self.b_1)
+        self.b_2 = QPushButton("2", self)
+        self.hbox_fourth.addWidget(self.b_2)
+        self.b_3 = QPushButton("3", self)
+        self.hbox_fourth.addWidget(self.b_3)
+        self.b_plus = QPushButton("+", self)
+        self.hbox_fourth.addWidget(self.b_plus)
+        
+        #5 row
+        self.b_00 = QPushButton("00", self)
+        self.hbox_fifth.addWidget(self.b_00)
+        self.b_0 = QPushButton("0", self)
+        self.hbox_fifth.addWidget(self.b_0)
+        self.b_dot = QPushButton(".", self)
+        self.hbox_fifth.addWidget(self.b_dot)
         self.b_result = QPushButton("=", self)
-        self.hbox_result.addWidget(self.b_result)
+        self.hbox_fifth.addWidget(self.b_result)
         
-        self.b_reset = QPushButton("reset", self)
-        self.hbox_result.addWidget(self.b_reset)
-
-        #задаем действия для кнопок
+        
+        #create button press action
         self.b_1.clicked.connect(lambda: self._button("1"))
         self.b_2.clicked.connect(lambda: self._button("2"))
         self.b_3.clicked.connect(lambda: self._button("3"))
@@ -69,36 +99,50 @@ class Calculator(QWidget):
         self.b_8.clicked.connect(lambda: self._button("8"))
         self.b_9.clicked.connect(lambda: self._button("9"))
         self.b_0.clicked.connect(lambda: self._button("0"))
-
+        self.b_00.clicked.connect(lambda: self._button("00"))
+        
         self.b_plus.clicked.connect(lambda: self._operation("+"))
         self.b_minus.clicked.connect(lambda: self._operation("-"))
         self.b_multiply.clicked.connect(lambda: self._operation("*"))
         self.b_divide.clicked.connect(lambda: self._operation("/"))
         
         self.b_reset.clicked.connect(self._reset)
-        
+        self.b_delete.clicked.connect(self._delete)
         self.b_result.clicked.connect(self._result)
-
-    #действие при нажатии на кнопки-цифры
+        self.b_percent.clicked.connect(lambda: self._button("%"))
+        self.b_dot.clicked.connect(lambda: self._button("."))
+    
+    
+    #create methods
     def _button(self, param):
         line = self.input.text()
-        self.input.setText(line + param)
-
-    #действие при нажатии на кнопки-операции
+        if param == "%":
+            self.input.setText(str(float(line) / 100))
+        elif param == ".":
+            if line:
+                self.input.setText(line + param)
+            else:
+                self.input.setText("0.")
+        else:
+            self.input.setText(line + param)
+    
     def _operation(self, op):
         if(self.input.text()):
-            self.num_1 = int(self.input.text())
+            self.num_1 = float(self.input.text())
             self.op = op
             self.input.setText("")
-
-    #действие при нажатии на кнопку reset (сброс)
+    
     def _reset(self):
         self.input.setText("")
-
-    #действие при нажатии на кнопку =
+    
+    def _delete(self):
+        line = self.input.text()
+        if line:
+            self.input.setText(line[0 : len(line) - 1])
+    
     def _result(self):
-        if(self.input.text()):
-            self.num_2 = int(self.input.text())
+        if self.input.text() and self.op:
+            self.num_2 = float(self.input.text())
             if self.op == "+":
                 self.input.setText(str(self.num_1 + self.num_2))
             elif self.op == "-":
@@ -111,7 +155,8 @@ class Calculator(QWidget):
             if self.op == "*":
                 self.input.setText(str(self.num_1 * self.num_2))
 
-#запуск
+
+#start app
 app = QApplication(sys.argv)
 
 win = Calculator()
